25201d61231d48b190d4e2bae32aa7d77ef988c7
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-04-02 17:13:54 +0800

    chore: run linters & formatters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,50 +6,48 @@
 	];
 </script>
 
-<div class="navbar sticky top-0 flex flex-wrap w-full justify-center items-center gap-3 bg-white shadow-md">
-	<div class="flex font-bold text-3xl text-foodpandapink px-5">
-		PSA
-	</div>
-	{#each sections as section}
+<div
+	class="navbar sticky top-0 flex w-full flex-wrap items-center justify-center gap-3 bg-white shadow-md"
+>
+	<div class="flex px-5 text-3xl font-bold text-foodpandapink">PSA</div>
+	{#each sections as section (section.name)}
 		<a
 			href={section.path}
-			class="text-foodpandagray hover:bg-foodpandapink hover:text-white rounded-sm p-4 transition-colors"
+			class="rounded-sm p-4 text-foodpandagray transition-colors hover:bg-foodpandapink hover:text-white"
 		>
 			{section.name}
 		</a>
 	{/each}
 </div>
 
-<div class ="text-foodpandablack p-4">
-
-
-	<h1 class="text-5xl font-bold text-foodpandapink text-center">Food Pangdamasa</h1>
+<div class="p-4 text-foodpandablack">
+	<h1 class="text-center text-5xl font-bold text-foodpandapink">Food Pangdamasa</h1>
 
 	<br />
 
-	<h2 class="text-2xl font-semibold text-center">SDG 2: Zero Hunger</h2>
+	<h2 class="text-center text-2xl font-semibold">SDG 2: Zero Hunger</h2>
 
 	<br />
 
-	<section id= "background" class="background scroll-mt-16">
+	<section id="background" class="background scroll-mt-16">
 		<h2 class="text-xl font-bold">Problem & Background</h2>
 		<p>
-			Foodpanda is an online food delivery platform that is widely used in the Philippines, especially
-			in recent years. Many customers turn to Foodpanda to have their food delivered from a range of
-			restaurants and stores available on the platform. Because of the nature of delivery services,
-			customers are still limited to the food options available in their local area. As such, users from
-			some cities may have convenient access to a wide range of highly satisfactory food while others
-			might not be able to avail of the same variety and quality in their area.
+			Foodpanda is an online food delivery platform that is widely used in the Philippines,
+			especially in recent years. Many customers turn to Foodpanda to have their food delivered from
+			a range of restaurants and stores available on the platform. Because of the nature of delivery
+			services, customers are still limited to the food options available in their local area. As
+			such, users from some cities may have convenient access to a wide range of highly satisfactory
+			food while others might not be able to avail of the same variety and quality in their area.
 		</p>
 
 		<br />
 
 		<h2 class="text-xl font-bold">Solution</h2>
 		<p>
-			To address this, a comprehensive analysis of the reviews left by Food Panda customers will be done
-			to gain a better understanding of food widely available and sought after in different cities. In
-			doing so, attention may be turned to areas that can still improve on the quality and variety of
-			available food on delivery service platforms such as Foodpanda.
+			To address this, a comprehensive analysis of the reviews left by Food Panda customers will be
+			done to gain a better understanding of food widely available and sought after in different
+			cities. In doing so, attention may be turned to areas that can still improve on the quality
+			and variety of available food on delivery service platforms such as Foodpanda.
 		</p>
 
 		<br />
@@ -61,8 +59,9 @@
 				restaurants/shops on delivery service platforms, such as Foodpanda?
 			</li>
 			<li>
-				What are the most commonly ordered cuisine/food/drink types (e.g. Fast Food, Filipino, etc.),
-				based on the amount of reviews & reviewers, in cities of varying levels of urbanization?
+				What are the most commonly ordered cuisine/food/drink types (e.g. Fast Food, Filipino,
+				etc.), based on the amount of reviews & reviewers, in cities of varying levels of
+				urbanization?
 			</li>
 		</ol>
 
@@ -70,10 +69,12 @@
 
 		<h2 class="text-xl font-bold">Research Hypotheses</h2>
 		<ol>
-			<li>Highly urbanized cities/regions are likely to have higher quality restaurants and shops.</li>
 			<li>
-				Users from highly urbanized cities/regions are likely to order more varied types of food, while
-				fast food may be more commonly ordered in other areas.
+				Highly urbanized cities/regions are likely to have higher quality restaurants and shops.
+			</li>
+			<li>
+				Users from highly urbanized cities/regions are likely to order more varied types of food,
+				while fast food may be more commonly ordered in other areas.
 			</li>
 		</ol>
 
@@ -82,8 +83,8 @@
 		<h2 class="text-xl font-bold">Objectives</h2>
 		<ol>
 			<li>
-				Determine if there is a correlation between the urbanization of a city/region to the quality of
-				the food they can order online.
+				Determine if there is a correlation between the urbanization of a city/region to the quality
+				of the food they can order online.
 			</li>
 			<li>Identify the types of food that Filipinos primarily order online.</li>
 		</ol>
@@ -116,12 +117,11 @@
 	<br />
 
 	<section id="about">
-		<h2 class="text-xl font-bold"> About Us </h2>
-		<div class="text-l flex gap-3 justify-center px-20 py-5 text-center">
+		<h2 class="text-xl font-bold">About Us</h2>
+		<div class="text-l flex justify-center gap-3 px-20 py-5 text-center">
 			<div class="w-full">DOMINGO, Karlo S.</div>
 			<div class="w-full">LAZARO, Benjeouelle A.</div>
 			<div class="w-full">MORACA, Phoebe Renee P.</div>
 		</div>
 	</section>
-
 </div>
